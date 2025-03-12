4af939d17fea9830bee5053632f3a3c0d5012722
Hayes Barber <you@example.com>
2025-03-12 18:21:03 -0400

    working on ball velocity

--- src/ball.rs
+++ src/ball.rs
@@ -6,13 +6,20 @@ pub struct BallPlugin;
 
 impl Plugin for BallPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup_ball);
+        app.add_systems(Startup, setup_ball)
+            .add_systems(Update, move_ball);
     }
 }
 
 #[derive(Component)]
-struct Ball {}
-const BALL_RADIUS: f32 = 15.;
+struct Ball;
+
+#[derive(Component)]
+struct Velocity(Vec2);
+
+const BALL_RADIUS: f32 = 12.;
+const BALL_SPEED: f32 = 300.0;
+
 fn setup_ball(
     mut commands: Commands,
     resolution: Res<resolution::Resolution>,
@@ -27,6 +34,15 @@ fn setup_ball(
             (resolution.screen_dimensions.y / 2.) - (ISLAND_HEIGHT * 3.),
             0.,
         )),
-        Ball {},
+        Ball,
+        Velocity(Vec2::new(1.0, -1.0).normalize() * BALL_SPEED),
     ));
+}
+
+fn move_ball(time: Res<Time>, mut query: Query<(&mut Transform, &Velocity), With<Ball>>) {
+    for (mut transform, velocity) in &mut query {
+        let delta = time.delta_secs();
+        transform.translation.x += velocity.0.x * delta;
+        transform.translation.y += velocity.0.y * delta;
+    }
 }
