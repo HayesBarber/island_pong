3ccbb1d347b74253b8d6e5fc802e19d88b2ee512
Hayes Barber <you@example.com>
2025-03-11 22:41:10 -0400

    exit app on Q

--- src/game.rs
+++ src/game.rs
@@ -6,9 +6,16 @@ pub struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins((player::PlayerPlugin, resolution::ResolutionPlugin))
-            .add_systems(Startup, setup_scene);
+            .add_systems(Startup, setup_scene)
+            .add_systems(Update, update_game);
     }
 }
 fn setup_scene(mut commands: Commands) {
     commands.spawn(Camera2d { ..default() });
+}
+
+fn update_game(keys: Res<ButtonInput<KeyCode>>, mut app_exit_events: EventWriter<AppExit>) {
+    if keys.pressed(KeyCode::KeyQ) {
+        app_exit_events.send(AppExit::Success);
+    }
 }
