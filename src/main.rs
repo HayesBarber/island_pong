dd6a201c8d2b7806b860cc7b5a9deaa3d690fbc1
Hayes Barber <you@example.com>
2025-03-11 21:22:45 -0400

    add window

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_systems(Update, hello_world_system).run();
-}
-
-fn hello_world_system() {
-    println!("hello world");
+    App::new()
+        .add_plugins((DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: String::from("Island Pong"),
+                    position: WindowPosition::Centered(MonitorSelection::Primary),
+                    resolution: Vec2::new(400., 512.).into(),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            })
+            .set(ImagePlugin::default_nearest()),))
+        .run();
 }
