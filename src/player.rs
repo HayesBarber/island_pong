cecc5d13e9490176ec0437168c64e180a150892b
Hayes Barber <you@example.com>
2025-03-11 22:15:37 -0400

    spawn player at bottom of screen

--- src/player.rs
+++ src/player.rs
@@ -1,3 +1,4 @@
+use crate::resolution;
 use bevy::prelude::*;
 
 pub struct PlayerPlugin;
@@ -7,10 +8,17 @@ impl Plugin for PlayerPlugin {
     }
 }
 
-fn setup_player(mut commands: Commands) {
-    commands.spawn((Sprite {
-        color: Color::srgb(1., 1., 1.),
-        custom_size: Some(Vec2::new(100.0, 20.0)),
-        ..default()
-    },));
+fn setup_player(mut commands: Commands, resolution: Res<resolution::Resolution>) {
+    commands.spawn((
+        Sprite {
+            color: Color::srgb(1., 1., 1.),
+            custom_size: Some(Vec2::new(100.0, 20.0)),
+            ..default()
+        },
+        Transform::from_translation(Vec3::new(
+            0.,
+            (-resolution.screen_dimensions.y / 2.) + 20.,
+            0.,
+        )),
+    ));
 }
