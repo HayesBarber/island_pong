54343bfb65fff35d8722f3bbd333e41ef0b27e46
Hayes Barber <you@example.com>
2025-03-11 22:32:09 -0400

    update player

--- src/player.rs
+++ src/player.rs
@@ -4,9 +4,13 @@ use bevy::prelude::*;
 pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup_player);
+        app.add_systems(Startup, setup_player)
+            .add_systems(Update, update_player);
     }
 }
+
+#[derive(Component)]
+struct Player {}
 
 fn setup_player(mut commands: Commands, resolution: Res<resolution::Resolution>) {
     commands.spawn((
@@ -20,5 +24,41 @@ fn setup_player(mut commands: Commands, resolution: Res<resolution::Resolution>)
             (-resolution.screen_dimensions.y / 2.) + 20.,
             0.,
         )),
+        Player {},
     ));
+}
+const SPEED: f32 = 400.;
+fn update_player(
+    mut player_query: Query<(&mut Player, &mut Transform)>,
+    time: Res<Time>,
+    keys: Res<ButtonInput<KeyCode>>,
+    resolution: Res<resolution::Resolution>,
+) {
+    let (mut _player, mut transform) = player_query.single_mut();
+
+    let mut horizontal = 0.;
+
+    if keys.pressed(KeyCode::KeyA)
+        || keys.pressed(KeyCode::KeyJ)
+        || keys.pressed(KeyCode::ArrowLeft)
+    {
+        horizontal += -1.;
+    }
+    if keys.pressed(KeyCode::KeyD)
+        || keys.pressed(KeyCode::KeyK)
+        || keys.pressed(KeyCode::ArrowRight)
+    {
+        horizontal += 1.;
+    }
+    transform.translation.x += horizontal * time.delta_secs() * SPEED;
+
+    let left_bound = (-resolution.screen_dimensions.x * 0.5) + 50.;
+    let right_bound = (resolution.screen_dimensions.x * 0.5) - 50.;
+
+    if transform.translation.x > right_bound {
+        transform.translation.x = right_bound;
+    }
+    if transform.translation.x < left_bound {
+        transform.translation.x = left_bound;
+    }
 }
