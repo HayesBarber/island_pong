a34a0392fbbddd61f21314d0bea20dbc25c978c0
Hayes Barber <you@example.com>
2025-03-15 15:33:52 -0400

    load save data

--- src/score.rs
+++ src/score.rs
@@ -9,25 +9,35 @@ pub struct ScorePlugin;
 
 impl Plugin for ScorePlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(load_score()).add_systems(
-            Update,
-            (update_score_display.run_if(resource_equals(GameState { running: true })),),
-        );
+        app.insert_resource(Score(0))
+            .insert_resource(load_save_data())
+            .add_systems(
+                Update,
+                (update_score_display.run_if(resource_equals(GameState { running: true })),),
+            );
     }
 }
 
-const SAVE_FILE: &str = "score.json";
-
+const SAVE_FILE: &str = "save_data.json";
 #[derive(Resource, Serialize, Deserialize)]
+pub struct SaveData {
+    high_score: i32,
+    last_score: i32,
+}
+
+#[derive(Resource)]
 pub struct Score(pub i32);
 
-fn load_score() -> Score {
+fn load_save_data() -> SaveData {
     if let Ok(data) = fs::read_to_string(SAVE_FILE) {
         if let Ok(save_data) = serde_json::from_str(&data) {
             return save_data;
         }
     }
-    Score(0)
+    SaveData {
+        high_score: -1,
+        last_score: -1,
+    }
 }
 
 #[derive(Component)]
