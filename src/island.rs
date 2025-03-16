a86ed92b95c9c0f2a58b2a41522e16b4e89f0df4
Hayes Barber <you@example.com>
2025-03-16 18:54:34 -0400

    make island a capsule

--- src/island.rs
+++ src/island.rs
@@ -17,20 +17,26 @@ pub const ISLAND_HEIGHT: f32 = 20.;
 fn spawn_island(
     mut commands: Commands,
     resolution: Res<resolution::Resolution>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
     mut game_start_events: EventReader<GameStartEvent>,
 ) {
     for _ in game_start_events.read() {
         commands.spawn((
-            Sprite {
-                color: Color::srgb(1., 1., 1.),
-                custom_size: Some(Vec2::new(ISLAND_WIDTH, ISLAND_HEIGHT)),
+            Mesh2d(meshes.add(Capsule2d::new(
+                ISLAND_HEIGHT / 2.,
+                ISLAND_WIDTH - ISLAND_HEIGHT / 2.,
+            ))),
+            MeshMaterial2d(materials.add(Color::srgb(1., 1., 1.))),
+            Transform {
+                translation: Vec3::new(
+                    0.,
+                    (resolution.screen_dimensions.y / 2.) - (ISLAND_HEIGHT / 2.) - 10.,
+                    0.,
+                ),
+                rotation: Quat::from_rotation_z(std::f32::consts::FRAC_PI_2),
                 ..default()
             },
-            Transform::from_translation(Vec3::new(
-                0.,
-                (resolution.screen_dimensions.y / 2.) - (ISLAND_HEIGHT / 2.) - 10.,
-                0.,
-            )),
             Island {},
         ));
     }
